aeaaae705cb9671b1d2b1d95d0149a8aed4c684b
erinmuzughi <erinmuzughi@gmail.com>
2023-10-13 10:27:22 -0500

    refactored to include fragments in search and list-jobs for common code and added comments

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -30,17 +30,25 @@ public class SearchController {
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm ) {
-        ArrayList<Job> jobSearchResults = new ArrayList<>();
+        //set up PostMapping to the results page which is consistent with the action of the form, using Model to pass
+        // the variables back to the view, and @RequestParam to pass the values we get from the form submission into this method.
+        ArrayList<Job> jobs; //stores the results from the if/else statements below so we can use them in the view later
 
-        if (searchTerm.equalsIgnoreCase("all") || searchTerm.isEmpty()) {
-           jobSearchResults = JobData.findAll();
+        if (searchTerm.equalsIgnoreCase("all") || searchTerm.isEmpty()) { //checks to see if the search bar
+            // is submitted with the word all in it regardless of case or if it's empty, if so, it calls the findAll()
+            // method from the JobData model, which pulls in all Job values for each category; the data returned is stored in the array
+           jobs = JobData.findAll();
 
         } else {
-            jobSearchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm); //otherwise, we call this method and pass in
+            // the searchType which is the category selected via the radio button and the searchTerm entered in the
+            // search bar; That method in the model then matches the searchType and executes the logic within that if
+            // statement to search for the searchTerm within that column of data; the data returned is stored in the array
         }
-        model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm);
-        model.addAttribute("columns", columnChoices);
-        model.addAttribute("jobSearchResults", jobSearchResults);
+        model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm); //dynamically
+        // adjusts the h1 title and passes it into the view to include the searchType and searchTerm when applicable
+        model.addAttribute("columns", columnChoices); //passes the columnChoices into the view
+        model.addAttribute("jobs", jobs); //passes the jobs array which contains the resuts of the users search into the view
         return "search";
     }
 
